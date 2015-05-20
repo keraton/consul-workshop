c88094c79b26d8d9a67e31a54c503c7384a36109
Sveinung Dalatun <sdalatun@gmail.com>
2015-05-20 19:14:23 +0200

    Blah

--- provision/install_consul.sh
+++ provision/install_consul.sh
@@ -2,14 +2,16 @@
 
 wget -q https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip
 
+sudo apt-get update
+
 sudo apt-get -y install unzip
 
 unzip 0.5.2_linux_amd64.zip
-sudo mkdir /usr/local/bin
+sudo mkdir -p /usr/local/bin
 sudo cp consul /usr/local/bin/
 sudo chmod 755 /usr/local/bin/consul
 
-sudo mkdir /etc/consul.d
-sudo chmod vagrant /etc/consul.d
+sudo mkdir -p /etc/consul.d
+sudo chown vagrant /etc/consul.d
 
 
