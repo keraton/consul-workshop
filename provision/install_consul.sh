1351242b4d77e27576a7d0382ec5ea50c4c4ff20
Bowie Brotosumpeno <keratonjava@gmail.com>
2017-01-26 23:47:11 +0100

    Update consul url

--- provision/install_consul.sh
+++ provision/install_consul.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-wget -q -nc https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip
+wget -q -nc https://releases.hashicorp.com/consul/0.7.3/consul_0.7.3_linux_amd64.zip
 
 sudo apt-get update
 
 sudo apt-get -y install unzip
 
-unzip 0.5.2_linux_amd64.zip
+unzip consul_0.7.3_linux_amd64.zip
 sudo mkdir -p /usr/local/bin
 sudo cp consul /usr/local/bin/
 sudo chmod 755 /usr/local/bin/consul
@@ -16,7 +16,7 @@ sudo chown vagrant /etc/consul.d
 
 ### Install Web-gui
 
-wget -q -nc https://dl.bintray.com/mitchellh/consul/0.5.2_web_ui.zip
+wget -q -nc https://releases.hashicorp.com/consul/0.7.3/consul_0.7.3_web_ui.zip
 sudo mkdir -p /opt/consul-web
-sudo unzip 0.5.2_web_ui.zip -d /opt/consul-web/
+sudo unzip consul_0.7.3_web_ui.zip -d /opt/consul-web/
 sudo chown -R vagrant /opt/consul-web/
