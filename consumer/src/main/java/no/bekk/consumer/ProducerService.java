eb28af588948f3cfc2fd134cad55a64dc278ba16
bbrotosumpeno <bbrotosumpeno@egencia.com>
2017-02-28 23:06:07 +0100

    solution Consul-Client

--- consumer/src/main/java/no/bekk/consumer/ProducerService.java
+++ consumer/src/main/java/no/bekk/consumer/ProducerService.java
@@ -1,8 +1,16 @@
 package no.bekk.consumer;
 
+import com.orbitz.consul.CatalogClient;
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.HealthClient;
+import com.orbitz.consul.model.catalog.CatalogService;
+import com.orbitz.consul.model.health.ServiceHealth;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import java.util.List;
+import java.util.Optional;
 
 public class ProducerService {
 
@@ -10,8 +18,22 @@ public class ProducerService {
 
     public ProducerService() {
         Client client = ClientBuilder.newClient();
-        target = client.target("http://172.20.100.6:8080")
-                .path("/");
+
+        // Using Consul Client
+        Consul consul = Consul.builder().build(); // connect to Consul on localhost
+        CatalogClient catalogClient = consul.catalogClient();
+
+        List<CatalogService> producer = catalogClient.getService("producer").getResponse();
+        Optional<CatalogService> catalogServiceOptional = producer.stream().findFirst();
+
+        if (catalogServiceOptional.isPresent()) {
+            CatalogService catalogService = catalogServiceOptional.get();
+            target = client.target("http://" + catalogService.getAddress() + ":" + catalogService.getServicePort());
+        }
+        else {
+            throw new IllegalStateException("No service is found");
+        }
+
     }
 
     public String produce() {
