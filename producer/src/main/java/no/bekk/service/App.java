520148142fa41da0c659f1e3f01e53118d74201b
bbrotosumpeno <bbrotosumpeno@egencia.com>
2017-02-28 23:37:46 +0100

    Add register and deregister

--- producer/src/main/java/no/bekk/service/App.java
+++ producer/src/main/java/no/bekk/service/App.java
@@ -1,10 +1,14 @@
 package no.bekk.service;
 
+import com.orbitz.consul.AgentClient;
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.model.agent.Registration;
 import io.dropwizard.Application;
+import io.dropwizard.lifecycle.Managed;
 import io.dropwizard.setup.Environment;
 
-public class App extends Application<AppConfig>
-{
+
+public class App extends Application<AppConfig> {
     public static void main(String[] args) throws Exception {
         new App().run(args);
     }
@@ -16,7 +20,33 @@ public class App extends Application<AppConfig>
         System.out.println("Service listening on " + ipAddress + ":" + port);
 
         environment.jersey().register(new ProducerResource(port, ipAddress));
-
         environment.healthChecks().register("produces", new ProducerHealthCheck());
+        environment.lifecycle().manage(new Managed() {
+
+            @Override
+            public void start() throws Exception {
+                // Register service
+                System.out.println("Register service");
+                Consul consul = Consul.builder().build(); // connect to Consul on localhost
+                AgentClient agentClient = consul.agentClient();
+
+                agentClient.register(8080, Registration.RegCheck.http("http://localhost:8081/ping", 10), "producer", "1");
+            }
+
+            @Override
+            public void stop() throws Exception {
+                // Deregister service
+                System.out.println("Deregister service");
+                Consul consul = Consul.builder().build(); // connect to Consul on localhost
+                AgentClient agentClient = consul.agentClient();
+
+                agentClient.deregister("producer");
+
+            }
+        });
+
+
+
     }
+
 }
